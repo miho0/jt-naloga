bfe99ce4cc9e5af638c040756f43ae4548c3b3f2
amadejpapez <63184600+amadejpapez@users.noreply.github.com>
2025-05-05 19:13:49 +0200

    cleanup, add implementation with nltk library

--- main_nltk.py
+++ main_nltk.py
@@ -1,12 +1,16 @@
+import collections
 import json
 import string
 
+import nltk
+from nltk import ngrams
+
 # https://www.kaggle.com/datasets/devdope/900k-spotify
 
-SUPPORTED_EMOTIONS = ["anger", "fear", "sadness", "joy"]
+nltk.download("punkt_tab")
 
-# EMOTION_SIZE_LIMIT = 25000
-GRAM_SIZE = 4
+SUPPORTED_EMOTIONS = ["anger", "fear", "sadness", "joy"]
+X_GRAMS = 4
 
 
 def clean_text(text):
@@ -19,9 +23,7 @@ def main():
     for emotion in SUPPORTED_EMOTIONS:
         lyrics[emotion] = []
 
-    counted_songs = 0
-
-    with open("dataset.json", 'r', encoding='utf-8') as file:
+    with open("dataset.json", "r", encoding="utf-8") as file:
         for line in file:
             json_data = json.loads(line)
             emotion = json_data["emotion"]
@@ -29,32 +31,20 @@ def main():
             if emotion not in SUPPORTED_EMOTIONS:
                 continue
 
-            # if len(lyrics[emotion]) >= EMOTION_SIZE_LIMIT:
-            #     continue
-
             lyrics[emotion].append(json_data["text"])
-            counted_songs += 1
-
-            # if counted_songs > EMOTION_SIZE_LIMIT * len(SUPPORTED_EMOTIONS):
-            #     break
 
     for emotion, lyrics in lyrics.items():
-        grams: dict[str, int] = {}
+        grams: collections.Counter = collections.Counter()
 
         for lyric in lyrics:
-            words = lyric.split()
-
-            for i in range(len(words) - (GRAM_SIZE - 1)):
-                gram = " ".join(words[i:i + GRAM_SIZE])
-
-                if gram not in grams:
-                    grams[gram] = 0
-
-                grams[gram] += 1
+            tokens = nltk.word_tokenize(clean_text(lyric))
+            songNgrams = ngrams(tokens, X_GRAMS)
+            songNgramsStr = [" ".join(gram) for gram in list(songNgrams)]
+            grams.update(songNgramsStr)
 
         sorted_dict = dict(sorted(grams.items(), key=lambda x: x[1], reverse=True))
 
-        with open("result_" + emotion + "_5.json", 'w') as fp:
+        with open("new_no_punc_result_" + emotion + "_" + str(X_GRAMS) + ".json", "w") as fp:
             json.dump(sorted_dict, fp)
 
 
